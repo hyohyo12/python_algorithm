c1c202dda012cf54428eec84238b38b3d4b7a664
hyohyo12 <ehdgy9999@naver.com>
2024-01-21 18:26:39 +0900

    [Feat] gitignore 추가 및 문제 풀기
    라면 사기(small) 다이아 , 그리디 알고리즘
    애너그램 골드 5 , 백트래킹

--- baekjoon/9465.py
+++ baekjoon/9465.py
@@ -3,11 +3,13 @@ def max_score(seq:list[list[int]],n:int)->int:
     dp[0][0] = seq[0][0]
     dp[1][0] = seq[1][0]
     if n == 1:
-        return max(dp[0][1],dp[1][0])
+        return max(dp[0][0],dp[1][0])
     dp[0][1] = seq[1][0]+seq[0][1]
     dp[1][1] = seq[0][0]+seq[1][1]
+    
     if n == 2:
         return max(dp[0][1],dp[1][1])
+    
     for i in range(2,n):
         dp[0][i] =max(dp[1][i-2],dp[1][i-1])+seq[0][i]
         dp[1][i] = max(dp[0][i-2],dp[0][i-1])+seq[1][i]
